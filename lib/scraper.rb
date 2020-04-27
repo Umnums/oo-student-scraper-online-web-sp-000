92b11fd51854352af3200d22ab314d15d0711ef4
Joseph Kao <josephtkao@gmail.com>
2020-04-27 15:49:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,17 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
+  def get_page(urL)
+    Nokogiri::HTML(open(url))
+  end
+  
   def self.scrape_index_page(index_url)
-    
+    doc = get_page(index_url)
+    final = []
+    print students = doc.css(".student-class")
   end
 
   def self.scrape_profile_page(profile_url)
